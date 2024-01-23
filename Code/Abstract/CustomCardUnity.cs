9e6a1da5b37a9c6297be6a1a54e0f083667d37ee
AALUND13 <tamlund1976@gmail.com>
2024-01-23 15:01:38 -0500

    Improve the readme

--- Code/Abstract/CustomCardUnity.cs
+++ Code/Abstract/CustomCardUnity.cs
@@ -1,9 +1,7 @@
 ﻿using ClassesManagerReborn;
-using ModdingUtils;
+using ClassesManagerReborn.Util;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
-using UnboundLib;
 using UnboundLib.Cards;
 using UnityEngine;
 
@@ -12,10 +10,7 @@ namespace JARL.Abstract
     public abstract class CustomCardUnity : CustomCard
     {
         [Header("Class Value")]
-        public bool isClass = false;
-        public bool showCardClassName = false;
-        public string className = null;
-
+        public bool automatedlyCreateClass = true;
 
         public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
         {
@@ -69,47 +64,30 @@ namespace JARL.Abstract
         /// </summary>
         private void CreateClassText()
         {
-            // Retrieve lists of class information for different card types
-            List<CardInfo> classes = ClassesRegistry.GetClassInfos(CardType.Entry).ToList();
-            List<CardInfo> subClasses = ClassesRegistry.GetClassInfos(CardType.SubClass).ToList();
-            List<CardInfo> classesCard = ClassesRegistry.GetClassInfos(~CardType.Entry).ToList();
-
-            // Get the ClassObject associated with the card
-            ClassObject cardClassObject = ClassesRegistry.Get(ModdingUtils.Utils.Cards.instance.GetCardWithName(cardInfo.cardName));
-
-            // Check if the card is a class or class card
-            if (classes.Any(classCardInfo => classCardInfo.cardName == cardInfo.cardName) || (classesCard.Any(classCardInfo => classCardInfo.cardName == cardInfo.cardName) && cardClassObject.RequiredClassesTree.FirstOrDefault()?.Count() > 0))
+            if (automatedlyCreateClass)
             {
-                // Determine the class name
-                string className = cardClassObject.RequiredClassesTree.FirstOrDefault()?.FirstOrDefault()?.cardName ?? "Class";
+                // Retrieve lists of class information for different card types
+                List<CardInfo> classes = ClassesRegistry.GetClassInfos(CardType.Entry).ToList();
+                List<CardInfo> subClasses = ClassesRegistry.GetClassInfos(CardType.SubClass).ToList();
+                List<CardInfo> classesCard = ClassesRegistry.GetClassInfos(~CardType.Entry).ToList();
 
-                // If the card is a subclass, set the class name to the subclass name
-                if (subClasses.Any(subClassCardInfo => subClassCardInfo.cardName == cardInfo.cardName))
-                    className = cardInfo.cardName;
+                // Get the ClassObject associated with the card
+                ClassObject cardClassObject = ClassesRegistry.Get(ModdingUtils.Utils.Cards.instance.GetCardWithName(cardInfo.cardName));
 
-                // Create and display the class name text
-                GameObject modNameObj = new GameObject("ClassText");
-
-                // Find the bottom-left edge object in the canvas hierarchy
-                RectTransform[] allChildrenRecursive = gameObject.GetComponentsInChildren<RectTransform>();
-                var edgeTransform = allChildrenRecursive.FirstOrDefault(obj => obj.gameObject.name == "EdgePart (1)");
-                if (edgeTransform != null)
+                // Check if the card is a class or subclass
+                if (classes.Any(classCardInfo => classCardInfo.cardName == cardInfo.cardName) || (classesCard.Any(classCardInfo => classCardInfo.cardName == cardInfo.cardName) && cardClassObject.RequiredClassesTree.FirstOrDefault()?.Count() > 0))
                 {
-                    GameObject bottomLeftCorner = edgeTransform.gameObject;
-                    modNameObj.gameObject.transform.SetParent(bottomLeftCorner.transform);
+                    // Determine the class name
+                    string className = cardClassObject.RequiredClassesTree.FirstOrDefault()?.FirstOrDefault()?.cardName ?? "Class";
+
+                    // If the card is a subclass, set the class name to the subclass name
+                    if (subClasses.Any(subClassCardInfo => subClassCardInfo.cardName == cardInfo.cardName))
+                        className = cardInfo.cardName;
+
+                    // Create and display the class name text
+                    ClassNameMono classNameMono = gameObject.AddComponent<ClassNameMono>();
+                    classNameMono.className = className;
                 }
-
-                // Add TextMeshProUGUI component for displaying text
-                TextMeshProUGUI modText = modNameObj.gameObject.AddComponent<TextMeshProUGUI>();
-                modText.text = className;
-
-                // Set the rotation, scale, and alignment of the text
-                modNameObj.transform.localEulerAngles = new Vector3(0f, 0f, 135f);
-                modNameObj.transform.localScale = Vector3.one;
-                modNameObj.AddComponent<SetLocalPos>();
-                modText.alignment = TextAlignmentOptions.Bottom;
-                modText.alpha = 0.1f;
-                modText.fontSize = 54;
             }
         }
 
--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# _JARL (Just Another Rounds Library)
+
+
+# JARL (Just Another Rounds Library) 1.0.0
+JARL (Just Another Rounds Library) is a versatile toolkit that introduces a `Armor Framework` and provides utilities like `CustomCardUnity` to streamline the creation of custom cards in Unity.
+
+# Features
+
+- **Armor Framework**: JARL integrates a flexible Armor Framework, empowering developers to craft their own armor or incorporate methods into armors created by other developers. This is achieved through the utilization of `DamageProcessingMethodsAfter` and `DamageProcessingMethodsBefore`, allowing for extensive customization.
+  
+-  **Custom Card Unity**: JARL also includes CustomCard for Unity, an class that can be inherited to create custom cards within the Unity environment.
+# Usage
+## Armor Framework
+
+### Creating a Armor Type
+You can create an armor type by inheriting from the `ArmorBase` class. Here is an example of an armor type:
+```csharp
+public class ExampleArmor : ArmorBase
+{
+    public override string GetArmorType()
+    {
+        return "Example";
+    }
+
+    public override BarColor GetBarColor()
+    {
+        return new BarColor(Color.cyan * 0.6f, Color.cyan * 0.45f);
+    }
+
+    public override int GetPriority()
+    {
+        return 1; 
+    }
+
+    public override void SetupArmor()
+    {
+        armorTags.Add("CanArmorPierce");
+        reactivateArmorType = ArmorReactivateType.Second;
+    }
+}
+```
+#### Registering Armor
+After creating your armor, you can register it using the `RegisterArmorType` method from the `ArmorFramework` class:
+```csharp
+void Start()
+{
+	ArmorFramework.RegisterArmorType(new ExampleArmor());
+}
+``` 
+#### Adding Armor To Players
+You can add armor to a player, for example, when a player picks a card, by using the `AddArmor` method from the `ArmorHandler` class:
+```csharp
+public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+{
+    player.data.GetAdditionalData().armorHandler.AddArmor("Example", 50, 5, 5, ArmorReactivateType.Second, 5);
+}
+```
+## Custom Card Unity
+### Creating a Custom Card Fr Unity
+You can create a custom card unity by inheriting from `CustomCardUnity`. Here is an example of a custom card for Unity:
+```csharp
+using JARL.Abstract;
+using JARL.Extensions;
+using UnityEngine;
+
+public class ExampleUnityCard : CustomCardUnity
+{
+    public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+    {
+		// Your Code Here
+    }
+
+    public override string GetModName()
+    {
+        return "Your Mod Initials";
+    }
+}
+```
+Replace placeholders like `[Your Mod Initials]` with your actual content.
+#
