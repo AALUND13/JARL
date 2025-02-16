1dbaee0abc0c890c5daa5ca3e99288fedf215903
AALUND13 <tamlund1976@gmail.com>
2025-02-16 03:41:12 -0500

    Added a category to block card draw until another player has armor

---
+++ Scripts/Patches/PlayerIsAllowedCardPatch.cs
@@ -0,0 +1,25 @@
+﻿using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using HarmonyLib;
+using JARL.Armor;
+using JARL.Extensions;
+using System.Linq;
+
+namespace JARL.Patches {
+    [HarmonyPatch(typeof(ModdingUtils.Utils.Cards))]
+    internal class PlayerIsAllowedCard {
+        public static CardCategory DrawWhenPlayerHaveArmorCategory = CustomCardCategories.instance.CardCategory("DrawWhenPlayerHaveArmor");
+
+        [HarmonyPatch("PlayerIsAllowedCard")]
+        [HarmonyPostfix]
+        public static void Postfix(Player player, CardInfo card, ref bool __result, ModdingUtils.Utils.Cards __instance) {
+            bool otherPlayersNotHaveArmor = PlayerManager.instance.players
+                .Where(p => p != player)
+                .All(p => ArmorFramework.ArmorHandlers[p].ActiveArmors.Count == 0);
+
+            // If other players don't have armor and the card is in the DrawWhenPlayerHaveArmor category, don't allow the card to be drawn
+            if(otherPlayersNotHaveArmor
+                && card.categories.Contains(DrawWhenPlayerHaveArmorCategory)
+            ) __result = false;
+        }
+    }
+}
